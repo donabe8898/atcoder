55ede7d81d8f692e5fa0b8257266685b228f2019
u5 <60559703+donabe8898@users.noreply.github.com>
2021-06-25 12:24:44 +0900

    Timposter

---
+++ ABC206/C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using ll = long long int;
+#define RES 2000000;
+using namespace std;
+int main (){
+	cin.tie(0);
+   ios_base::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	vector<int> a;
+	for(int i=0;i<n;i++){
+		cin>>a.at(i);
+	}
+	
+	return 0;
+}
