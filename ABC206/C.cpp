33723961a435b4c4e7510c53ad35705e41c86d7c
u5 <60559703+donabe8898@users.noreply.github.com>
2021-06-26 22:22:56 +0900

    abc206

--- ABC206/C.cpp
+++ ABC206/C.cpp
@@ -7,10 +7,22 @@ int main (){
    ios_base::sync_with_stdio(false);
 	int n;
 	cin>>n;
-	vector<int> a;
+	vector<int> a(301010);
 	for(int i=0;i<n;i++){
 		cin>>a.at(i);
 	}
-	
+	map<int,int> m;
+	int ans = 0;
+	for(int j=0;j<n;j++){
+		ans += j - m[a[j]];
+		m[a[j]]++;
+	}
+	cout<<ans<<endl;
+
 	return 0;
 }
+
+/*
+	解説: https://blog.hamayanhamayan.com/entry/2021/06/19/233714
+	map:  https://cpprefjp.github.io/reference/map/map.html
+*/
