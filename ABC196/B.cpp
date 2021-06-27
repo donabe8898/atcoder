581f05769e61ce7cd14b67b3d63ed4fc19862136
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2021-06-27 22:39:12 +0900

    B問題とポインタをちょっと

---
+++ ABC196/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using ll = long long int;
+#define RES 2000000;
+using namespace std;
+int main (){
+	cin.tie(0);
+   ios_base::sync_with_stdio(false);
+   string s,ans;
+   cin>>s;
+   for(auto a:s){
+     if(a == '.'){
+       break;
+     }
+     ans.push_back(a);
+   }
+   cout<< ans<<endl;
+	return 0;
+}
---
+++ pointer.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using ll = long long int;
+#define RES 2000000;
+using namespace std;
+int main (){
+	cin.tie(0);
+   ios_base::sync_with_stdio(false);
+
+   int x = 2;
+   int *a;    //ポインタを定義
+   a = &x;    //aにはxのアドレスを書き込む
+   *a = 3;    //aに書き込んだアドレス元を書き換え
+   cout<<x<<endl;
+
+   int y;
+   y = *a;
+   cout<<y<<endl;
+	return 0;
+}
