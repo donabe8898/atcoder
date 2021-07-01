3d3dfbcda61461f9bbc53e5168e595824503e8a9
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2021-07-01 22:49:09 +0900

    7/1

---
+++ ABC204/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define int long long
+#define endl "\n"
+#define all(n) n.begin(),n.end()
+#define rall(n) n.rbegin(),n.rend()
+#define rep(i, s, n) for (int i = s; i < (int)(n); i++)
+#define floatset(n) fixed<<setprecision(n)
+#define rangeout(x,y,h,w) (x<0||y<0||h-1<x||w-1<y)
+using namespace std;
+using namespace atcoder;
+template<class T>using vec=vector<T>;
+template<class T>using min_queue=priority_queue<T,vector<T>,greater<T>>;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+using Graph = vector<vector<int>>;
+struct Edge1{ll to,cost;};
+using WGraph = vec<vec<Edge1>>;
+struct BellEdge{ll from,to,cost;};
+using BGraph=vec<BellEdge>;
+constexpr ll INF = 1e18;
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int dx[4]={1,0,-1,0};
+constexpr int dy[4]={0,1,0,-1};
+template<typename T>
+bool chmin(T&a,T b){return a>b?a=b,true:false;}
+template<typename T>
+bool chmax(T&a,T b){return a<b?a=b,true:false;}
+bool Flag(int bit,int i){return (bit&(1LL<<i));}
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    fill( (T*)array, (T*)(array+N), val );
+}
+signed main (){
+		cin.tie(0);
+		ios_base::sync_with_stdio(false);
+    int N,ans=0;
+    cin>>N;
+    vector<int> A(N);
+    rep(i,0,N){
+      cin>>A.at(i);
+    }
+    for(int &i:A){
+      if(i>10){
+        ans += i - 10;
+      }
+    }
+    cout<<ans<<endl;
+
+		return 0;
+}
