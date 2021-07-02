65369fec98e0013ab65179d6679b68a1a860886b
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2021-07-02 10:49:34 +0900

    7/2

---
+++ ABC203/B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define all(n) n.begin(),n.end()
+#define endl "\n"
+#define rall(n) n.rbegin(),n.rend()
+#define rep(i, s, n) for (int i = s; i < (int)(n); i++)
+#define floatset(n) fixed<<setprecision(n)
+#define rangeout(x,y,h,w) (x<0||y<0||h-1<x||w-1<y)
+using namespace std;
+using namespace atcoder;
+template<class T>using vec=vector<T>;
+template<class T>using min_queue=priority_queue<T,vector<T>,greater<T>>;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+using Graph = vector<vector<int>>;
+struct Edge1{ll to,cost;};
+using WGraph = vec<vec<Edge1>>;
+struct BellEdge{ll from,to,cost;};
+using BGraph=vec<BellEdge>;
+constexpr ll INF = 1e18;
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int dx[4]={1,0,-1,0};
+constexpr int dy[4]={0,1,0,-1};
+template<typename T>
+bool chmin(T&a,T b){return a>b?a=b,true:false;}
+template<typename T>
+bool chmax(T&a,T b){return a<b?a=b,true:false;}
+bool Flag(int bit,int i){return (bit&(1LL<<i));}
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    fill( (T*)array, (T*)(array+N), val );
+}
+signed main (){
+		cin.tie(0);
+		ios_base::sync_with_stdio(false);
+
+		string N,K,S;
+		cin>>N>>K;
+		int n=stoi(N),k=stoi(K);
+		int ans=0;
+		rep(i,1,n+1){
+			rep(j,1,k+1){
+				S = to_string(i) + '0' + to_string(j);
+				ans += stoi(S);
+			}
+		}
+		cout<<ans<<endl;
+		return 0;
+}
--- pointer.cpp
+++ pointer.cpp
@@ -5,15 +5,22 @@ using namespace std;
 int main (){
 	cin.tie(0);
    ios_base::sync_with_stdio(false);
+	/*
+	int x=2;
+	int a;
+	a = x;
+	a = 3;
+	cout<<x<<endl;
+	*/
 
-   int x = 2;
-   int *a;    //ポインタを定義
-   a = &x;    //aにはxのアドレスを書き込む
-   *a = 3;    //aに書き込んだアドレス元を書き換え
-   cout<<x<<endl;
+   int xx = 2;
+   int *aa;    //ポインタを定義
+   aa = &xx;    //aにはxのアドレスを書き込む
+   *aa = 3;    //aに書き込んだアドレス元を書き換え
+   cout<<xx<<endl;
 
    int y;
-   y = *a;    //aの指すアドレスの値を代入
+   y = *aa;    //aの指すアドレスの値を代入
    cout<<y<<endl;
 	return 0;
 }
