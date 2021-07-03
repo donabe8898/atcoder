668977067ad287046bb78b8ca15f88ab5c36e9ad
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2021-07-03 23:48:18 +0900

    今日はいったん休み

--- TEST.cpp
+++ TEST.cpp
@@ -1,15 +1,54 @@
 #include <bits/stdc++.h>
-using ll = long long int;
-#define RES 2000000;
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#include <atcoder/all>
+#define endl "\n"
+#define all(n) n.begin(),n.end()
+#define rall(n) n.rbegin(),n.rend()
+#define rep(i, s, n) for (int i = s; i < (int)(n); i++)
+#define floatset(n) fixed<<setprecision(n)
+#define rangeout(x,y,h,w) (x<0||y<0||h-1<x||w-1<y)
 using namespace std;
+using namespace atcoder;
+template<class T>using vec=vector<T>;
+template<class T>using min_queue=priority_queue<T,vector<T>,greater<T>>;
+using ll = long long;
+using ld = long double;
+using pll = pair<ll,ll>;
+using Graph = vector<vector<int>>;
+struct Edge1{ll to,cost;};
+using WGraph = vec<vec<Edge1>>;
+struct BellEdge{ll from,to,cost;};
+using BGraph=vec<BellEdge>;
+constexpr ll INF = 1e18;
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int dx[4]={1,0,-1,0};
+constexpr int dy[4]={0,1,0,-1};
+template<typename T>
+bool chmin(T&a,T b){return a>b?a=b,true:false;}
+template<typename T>
+bool chmax(T&a,T b){return a<b?a=b,true:false;}
+bool Flag(int bit,int i){return (bit&(1LL<<i));}
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    fill( (T*)array, (T*)(array+N), val );
+}
 int main (){
 		cin.tie(0);
-  	ios_base::sync_with_stdio(false);
-   int n=1;
-	do{
-		cout<<n;
-		n++;
-	}while(n<1);
-	 return 0;
+		ios_base::sync_with_stdio(false);
+   	int a[10][10];
+		int sum=0;
+		
+		for(int i = 0;i<10;i++){
+			for(int j = 0;j<10;j++){
+				a[i][j]=i+j;
+			}
+		}
+
+		for(int i = 0;i<10;i++){
+			for(int j = 0;j<10;j++){
+				if(i%2==j%3) sum+=a[i][j];
+			}
+		}
+		cout<<sum; // 305
+	 	return 0;
 }
