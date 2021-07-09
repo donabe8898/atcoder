4b14a50a9ef816f509db346564aa14052ab45906
u5 <60559703+donabe8898@users.noreply.github.com>
2021-07-09 16:34:21 +0900

    addBFS

--- BFS/BFS.cpp
+++ BFS/BFS.cpp
@@ -1,5 +1,5 @@
 /*
-	幅優先探索(BFS)
+	幅優先探索(depth-first search:BFS)
 	特徴：各辺の重みが等しいという条件のもと、最短経路を求められる。
 	応用：パズルを解くための最小手数を求める。
 	ノードからノードへ何ステップで行けるかを求める。
@@ -22,19 +22,20 @@ int main(){
 		G[b].push_back(a);
 	}
 	//全頂点を未訪問に設定
-	vector<int> dist(N,-1);//dist[v]はスタートからvまでのステップが格納される。
-	queue<int> Q;
+	vector<int> dist(N,-1);	//dist[v]はスタートからvまでのステップが格納される。
+	queue<int> Q;				//キュー
 
 	dist[0]=0;//この場合、頂点を0としている
 	Q.push(0);//頂点0 を訪問予定にする
+
 	//BFS開始（キューが空になるまで実行）
 	while(!Q.empty()){
 		int v = Q.front();//キューから先頭頂点を取り出す
 		Q.pop();
 		//vから辿れる全ノードを探索
 		for(int gv:G[v]){
-			if(dist[gv] != 1) continue; // 訪問済みなら飛ばす
-			dist[gv] = dist[gv]+1;//未訪問なら更新&キューに追加
+			if(dist[gv] != 1) continue; 	// 訪問済みなら飛ばす
+			dist[gv] = dist[gv]+1;			//未訪問なら更新&キューに追加
 			Q.push(gv);
 		}
 	}
