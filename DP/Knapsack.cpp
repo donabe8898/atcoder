2322b8e9a8c787536659d47970ac3ce840590bc2
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2021-07-15 23:28:00 +0900

    a

---
+++ DP/Knapsack.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<climits>
+#include<algorithm>
+using namespace std;
+template<class T> void chmax(T &a,T b){
+  if(a<b){
+    a=b;
+  }
+}
+
+int main(){
+  long long N,W; cin>>N>>W;
+  vector<long long> weight(N),value(N);
+  for(int i=0;i<N;i++)cin>>weight[i]>>value[i];
+  //DPテーブル定義
+  vector<vector<long long>> dp(N+1,vector<long long>(W+1,0));
+  //DP Loop
+  for(int i=0;i<N;i++){
+    for(int w=0;w<=W;w++){
+      //i番目の品物を選ぶ場合
+      if(w-weight[i]>=0) chmax(dp[i+1][w],dp[i][w-weight[i]]+value[i]);
+      //i番目の品物を選ばない場合
+      chmax(dp[i+1][w],dp[i][w]);
+    }
+  }
+  //最適値出力
+  cout<<dp[N][W]<<endl;
+  return 0;
+}
--- contest.cpp
+++ contest.cpp
@@ -45,33 +45,6 @@ int fh(int k){
 int main (){
 		cin.tie(0);
 		ios_base::sync_with_stdio(false);
-    //頂点Nと辺の数M
-  	ll N,Q; cin>>N>>Q;
-  	Graph2 G(N);
-  	//グラフ入力
-  	rep(i,N-1){
-  		int a,b; cin>>a>>b;
-  		G[a].push_back(b);
-  		//無向グラフの場合には以下を追加
-  		G[b].push_back(a);
-  	}
-  	//全頂点を未訪問に設定
-  	vector<int> dist(N,-1);	//dist[v]はスタートからvまでのステップが格納される。
-  	queue<int> QQ;				//キュー
 
-  	dist[0]=0;//この場合、頂点を0としている
-  	QQ.push(0);//頂点0 を訪問予定にする
-
-  	//BFS開始（キューが空になるまで実行）
-  	while(!QQ.empty()){
-  		int v = QQ.front();//キューから先頭頂点を取り出す
-  		QQ.pop();
-  		//vから辿れる全ノードを探索
-  		for(int gv:G[v]){
-  			if(dist[gv] != 1) continue; 	// 訪問済みなら飛ばす
-  			dist[gv] = dist[gv]+1;			//未訪問なら更新&キューに追加
-  			QQ.push(gv);
-  		}
-  	}
 		return 0;
 }
