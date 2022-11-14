7b2a61ab256e310cc874a4ce1d71e0640563aab2
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2022-11-14 14:24:27 +0900

    ABC265-B続き

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// ABC251-B
+// ABC265-B
 #include <bits/stdc++.h>
 #define _GLIBCXX_DEBUG
 #define rep(i, a, b) for (int i = a; i < b; ++i)
@@ -34,4 +34,50 @@ using Dp = vector<vector<ll>>;
  *      ・sortからのlower_bound
  */
 
-int main() { return 0; }
+int main() {
+  // input
+  ll N_rooms, M_bonus, Time;
+  cin >> N_rooms >> M_bonus >> Time;
+  vector<ll> A(N_rooms - 1);
+  map<ll, ll> XY;
+  queue<ll> X;
+  for (ll i = 0; i < N_rooms - 1; i++)
+    cin >> A[i];
+
+  for (ll j = 0; j < M_bonus; j++) {
+    ll x, y;
+    cin >> x >> y;
+    XY[x] = y;
+    X.push(x);
+  }
+  /**
+   * 持ち時間チェック
+   * マイナス
+   * queue
+   * map
+   * queue pop
+   * 2 kara kaishi
+   *
+   * Test
+   * 6 2 10
+   * 2 2 3 2 1
+   * 4 4
+   * 5 1
+   */
+  for (ll idx = 0; idx < N_rooms - 1; idx++) {
+    if (Time - A[idx] < 0) { // ng ng ng ng ng
+      cout << "No" << endl;
+      return 0;
+    }
+    // X(4,5)
+    Time -= A[idx];               // 10-2 8-2 6-3 7-2 6-1
+    if (!X.empty()) {             // ok ok ok ok ng
+      if (idx + 2 == X.front()) { // ng ng ok ok
+        Time += XY.at(X.front()); // - - 3+=4 5+=1
+        X.pop();
+      }
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
