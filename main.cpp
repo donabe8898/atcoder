71a4dc36e1001571301ea2027b1cbfec2d34f28c
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2022-11-22 11:03:20 +0900

    246やる

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// ABC265-B
+// ABC246-C
 #include <bits/stdc++.h>
 #define _GLIBCXX_DEBUG
 #define rep(i, a, b) for (int i = a; i < b; ++i)
@@ -34,23 +34,10 @@ using Dp = vector<vector<ll>>;
  *      ・sortからのlower_bound
  */
 
-int main() {
-  ll N_room, M_bonuses, T_time;
-  cin >> N_room >> M_bonuses >> T_time;
-  vector<ll> A(N_room - 1);
-  rep(i, 0, N_room - 1) cin >> A[i];
-  ll X;
-  vector<ll> Y(N_room, 0);
-  rep(i, 0, M_bonuses) cin >> X >> Y[X - 2];
+/**
+ * 降順にして前から削る
+ * 残りの累積和を取ってまたsort
+ * 削る
+ */
 
-  rep(idx, 0, N_room - 1) {
-    if (T_time - A[idx] <= 0) {
-      cout << "No" << endl;
-      return 0;
-    }
-    T_time -= A[idx];
-    T_time += Y[idx];
-  }
-  cout << "Yes" << endl;
-  return 0;
-}
+int main() { return 0; }
