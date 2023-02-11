52129085ae1a38a8ae2d2826022eacc26f77f9ac
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-02-11 20:57:29 +0900

    union find

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // ABC246-C
 #include <bits/stdc++.h>
+#include <cstdint>
 #define _GLIBCXX_DEBUG
 #define rep(i, a, b) for (int i = a; i < b; ++i)
 #define rrep(i, a, b) for (int i = a; i >= b; i--)
@@ -34,37 +35,45 @@ using Dp = vector<vector<ll>>;
  *      ・sortからのlower_bound
  */
 
-/**
- * 降順にして前から削る
- * 残りの累積和を取ってまたsort
- * 削る
- */
+class UnionFind {
+public:
+  vector<int> Parent;
+  UnionFind(int N) { Parent = vector<int>(N, -1); }
 
-int main() {
-  ll n, k, x;
-  cin >> n >> k >> x;
-  vector<ll> a(n);
-  ll sum = 0;
-  rep(i, 0, n) {
-    cin >> a[i];
-    sum += a[i];
+  int root(int A) {
+    if (Parent[A] < 0) {
+      return A;
+    }
+    return Parent[A] = root(Parent[A]);
   }
-  ll ksum = 0;
-  ll mod_sum = 0;
-  rep(i, 0, n) {
-    ksum += a[i] / x;
-    a[i] %= x;
-    mod_sum += a[i] % x;
-  }
-  if (ksum >= k) {
-    cout << sum - k * x << endl;
-    return 0;
+  // 自分のいるグループの頂点数を調べる
+  int size(int A) {
+    return -Parent[root(A)]; // 親をとってきたい]
   }
 
-  k -= ksum;
-  sort(a.begin(), a.end(), greater<>());
-  rep(i, 0, min(k, n)) { mod_sum -= a[i]; }
-  cout << mod_sum << endl;
+  // AとBをくっ付ける
+  bool connect(int A, int B) {
+    // AとBを直接つなぐのではなく、root(A)にroot(B)をくっつける
+    A = root(A);
+    B = root(B);
+    if (A == B) {
+      // すでにくっついてるからくっ付けない
+      return false;
+    }
 
-  return 0;
-}
+    // 大きい方(A)に小さいほう(B)をくっ付ける
+    // 大小が逆だったらひっくり返す
+    if (size(A) < size(B)) {
+      swap(A, B);
+    }
+
+    // Aのサイズを更新する
+    Parent[A] += Parent[B];
+    // Bの親をAに変更する
+    Parent[B] = A;
+
+    return true;
+  }
+};
+
+int main() { return 0; }
